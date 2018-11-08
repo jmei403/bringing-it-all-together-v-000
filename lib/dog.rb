007242ecb6cda411e426fcdb42605f0ed9be693a
Jason Mei <jason.mei403@gmail.com>
2018-11-08 20:45:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(id: nil, name:, breed:)
+  def initialize(id: nil, name: nil, breed: nil)
     @id = id
     @name = name
     @breed = breed
@@ -51,8 +51,8 @@ class Dog
     DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
   end
 
-  def self.create(name:, breed:)
-    dog = self.new(name, breed)
+  def self.create(hash)
+    dog = self.new
     dog.save
     dog
   end
