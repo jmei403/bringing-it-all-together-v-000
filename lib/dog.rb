5ca2114fa8284f3f1719714a72d21f4698610b0e
Jason Mei <jason.mei403@gmail.com>
2018-11-08 20:17:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,7 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?;"
-    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql, name).map { |row| self.new_from_db(row) }.first
   end
 
   # def self.create(name, breed)
